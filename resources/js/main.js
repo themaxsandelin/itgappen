8c1b23333553cbb383af096ebec900a57194bea7
Max Sandelin <themaxsandelin@gmail.com>
2016-05-31 20:15:04 +0200

    Added render support for iOS for padding to accomodate for the overlaying status bar in iOS. Added better touch support in click events by removing the 300ms delay.

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,3 +2,8 @@
 	// Set default moment locale (lang) so Swedish
 	moment.locale('sv');
 
+	if ('addEventListener' in document) {
+		document.addEventListener('DOMContentLoaded', function() {
+	    FastClick.attach(document.body);
+		}, false);
+	}
